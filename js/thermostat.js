4ab861791e3116cde411100a9b733435ba94aae9
Ptolemy <ptolemybarnes@gmail.com>
2015-01-20 23:59:20 +0000

    Initial, late, commit.

---
+++ js/thermostat.js
@@ -0,0 +1,36 @@
+var Thermostat = function() {};
+
+Thermostat.prototype.temperature     = 20;
+
+Thermostat.prototype.minTemp      = 10;
+
+Thermostat.prototype.maxTemp    = function() {
+  if (this.isPowerSavingOn)
+    return 25;
+  else
+    return 32;
+};
+
+Thermostat.prototype.isPowerSavingOn  = false;
+
+Thermostat.prototype.increaseTempBy = function(amount) {
+  var newTemperature = this.temperature + amount;
+  this.regulateTemp(newTemperature);
+  this.temperature = newTemperature;
+};
+
+Thermostat.prototype.decreaseTempBy = function(amount) {
+  var newTemperature = this.temperature - amount;
+  this.regulateTemp(newTemperature);
+  this.temperature = newTemperature;
+};
+
+Thermostat.prototype.regulateTemp = function(temp) {
+  if (temp < this.minTemp) {
+    throw Error('Temperature cannot go below ' + this.minTemp + '.');
+  }
+  else if (temp > this.maxTemp) {
+    throw Error('Temperature cannot go above ' + this.maxTemp + '.');
+  }
+};
+
---
+++ spec/thermostatSpec.js
@@ -0,0 +1,64 @@
+// Thermostat starts at 20 degrees
+// You can increase the temp with the up button
+// You can decrease the temp with the down button
+// The minimum temperature is 10 degrees
+// If power saving mode is on, the maximum temperature is 25 degrees
+// If power saving mode is off, the maximum temperature is 32 degrees
+// Power saving mode is on by default
+// You can reset the temperature to 20 by hitting the reset button
+// thermostat should color the display based on energy usage - < 18 is green, < 25 is yellow, otherwise red
+
+describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {thermostat = new Thermostat()});
+
+  describe("starts", function() {
+
+    it("that defaults to 20", function() {
+      
+      expect(thermostat.temperature).toBe(20);
+    })
+    
+    it("with default minimum temperature of 10", function() {
+      expect(thermostat.minTemp).toBe(10);
+    });
+
+    it("with default power saving mode off", function() {
+      expect(thermostat.isPowerSavingOn).toBe(false)
+    });
+  });
+
+  describe("temperature can", function() {
+    
+    it("be increased", function(){
+      thermostat.increaseTempBy(5);
+
+      expect(thermostat.temperature).toBe(25);
+    });
+
+    it('be decreased', function() {
+      thermostat.decreaseTempBy(6);
+
+      expect(thermostat.temperature).toBe(14);
+    });
+
+    it('not go below 10', function() {
+      expect( function(){ thermostat.decreaseTempBy(12); }).toThrowError("Temperature cannot go below 10.");
+    });
+  });
+
+  describe("when power saving mode", function() {
+
+    it("is off, max temperature is 32 degrees", function() {
+      expect(thermostat.maxTemp).toBe(32);
+      // expect( function(){ thermostat.increaseTempBy(15) }).toThrowError("Temperature cannot go above 32.");
+    });
+  });
+});
+
+
+
+
+
+

ebfb6a4bf8bfc0415d533f0627c8ae8f16d4c994
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-22 16:17:15 +0000

    Color responsive depending on temperature

--- public/js/script-jq.js
+++ public/js/script-jq.js
@@ -2,6 +2,16 @@ var thermostat = new Thermostat();
 
 $(document).on('click', function() {
   $("#temp-reading").text(thermostat.temperature);
+  $("h1").attr("class",thermostat.usageIndicator());
+  if (thermostat.usageIndicator()=='medium-usage') 
+    color = "blue"
+  else if (thermostat.usageIndicator()=='low-usage') 
+    color = "green"
+  else 
+    color = "red"
+  $("#power-saving-divider").css("background",color);
+  // $("#right").css("background-color",color);
+  // $("#power-saving-divider").attr("class",thermostat.usageIndicator());
 });
 
 $(document).ready(function() {
